f8853aa23eeca87717c2a3579cf73bf54ce425e6
Pavel <pashakonoplyanikov@gmail.com>
2020-08-02 21:51:07 +0300

    add cout word

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,6 +8,14 @@ def home(request):
 def reverse(request):
 	user_text=request.GET['messange']
 	reverse_text=user_text[::-1]
+	# count_word=0
+	# for i in user_text:
+	# 	if i==' ' :
+	# 		count_word = count_word+1
+	# count_word +=1
+	words=user_text.split()
+	number=len(words)
+	
 	return render(request,'reverse.html',{'usertext':user_text,
-		'reversetext':reverse_text})
+		'reversetext':reverse_text,'count':number})
 
